f0b34b17d71ea92068389f7b368ed42c116587a4
santiago alan <santiagoalan1@gmail.com>
2020-11-04 18:22:30 -0600

    added login

--- toDoList/src/app/pages/login/login.page.ts
+++ toDoList/src/app/pages/login/login.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { SessionService } from 'src/app/services/session/session.service';
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { TokenStorageService } from 'src/app/services/auth/token-storage.service';
+import { TasksService } from 'src/app/services/tasks/tasks.service';
 import { environment } from 'src/environments/environment';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +13,34 @@ import { environment } from 'src/environments/environment';
 })
 export class LoginPage implements OnInit {
 
-    user: string = '';  
-    password: string = '';  
-    constructor(private sessionService: SessionService) { }
-
+    username: string = 'alan'
+    password: string = 'laredo'
+    constructor(private authService: AuthService, 
+        private tokenStorageService: TokenStorageService,
+        public alertController: AlertController,
+        public toastController: ToastController,
+        private router: Router) { }
+    
     ngOnInit() {
     }
-
+    
     login(): void {
-        this.sessionService.login(this.user, this.password)
-    }
 
+        //activar loading
+        let username = this.username
+        let password = this.password
+        let toggleOrder : boolean = false
+        this.authService.login(username, password)
+        .then((cb)=>{
+            this.router.navigate(['/tasks']);
+            this.tokenStorageService.saveToken(cb.token);
+            this.tokenStorageService.saveUser(cb.user);            
+        }).catch(async (cb)=> {
+            const toast = await this.toastController.create({
+                message: cb.error.err.message,
+                duration: 1000
+            });
+            await toast.present() 
+        })        
+    }
 }
