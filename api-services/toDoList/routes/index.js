a9600d1ecfc1d4401e4947cf616b529c979792aa
santiago alan <santiagoalan1@gmail.com>
2020-11-07 21:00:44 -0600

    Se genera un servicio para los usuarios

--- api-services/toDoList/routes/index.js
+++ api-services/toDoList/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var authentication = require('./auth')
 var tasks = require('./tasks')
+var users = require('./users')
 
 const app = express()
 
 app.use('/auth', authentication)
 app.use('/tasks', tasks)
+app.use('/users', users)
 
 
 module.exports = app;
---
+++ api-services/toDoList/routes/users.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const User = require('../models/user.model');
+
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.end('Welcome to You in users Page')
+});
+
+router.put('/', function (req, res) {
+    let body = req.body;
+    let { username, password } = body;
+
+    let user = new User({
+        username,
+        password: bcrypt.hashSync(password, 10)
+    });
+
+    user.save((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                err,
+            });
+        }
+        res.json({
+            success: true,
+            message: "Se ha creado un nuevo usuario"
+        });
+    })
+});
+
+router.get('/checkusername/:username', function(req, res, next) {
+    let {username} = req.params;
+
+    User.findOne({ username: username }, cbExistsUser)
+
+    function cbExistsUser(erro, user) {
+        if (erro) {
+            return res.status(500).json({
+                success: false, 
+                err: erro
+            })
+        }
+        if (!user) {
+            return res.json({
+                success: true,
+                exists: false,
+                message: "Nombre de usuario disponible"
+            })
+        }
+        res.json({ success: true, exists: true, message: "El nombre de usuario ya esta en uso"})
+    }
+});
+
+module.exports = router;
